60004dd3b1445aec928924902efa3ba1b0a4da59
Sperry <1080333@macomb.edu>
2023-09-18 14:33:58 -0400

    ;)

---
+++ smile.py
@@ -0,0 +1,7 @@
+print("You don't know what I'm doing :)")
+userinput = str
+userinput = input()
+if userinput == "I know what you're doing John":
+    print("Nuh uh ;)")
+else:
+    print("I was bored... I'll do something funny later")
